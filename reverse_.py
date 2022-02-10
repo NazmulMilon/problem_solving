edc58f78e79aee3988c2dd3b385b3f37b71c219a
nazmulmilon <milonsarker73@gmail.com>
2022-02-10 16:22:31 +0600

    reverse number using function

--- reverse_.py
+++ reverse_.py
@@ -1,3 +1,4 @@
+'''
 number = int(input("Enter a number: "))
 reverse=0
 while number!=0:
@@ -5,4 +6,14 @@ while number!=0:
     reverse=reverse*10+digit
     number=number//10
 print(reverse)
+'''
 
+def reverse(number):
+    re=0
+    while number!=0:
+        digit=number%10
+        re=re*10+digit
+        number=number//10
+    return re
+number=int(input("Enter a number: "))
+print(reverse(number))
