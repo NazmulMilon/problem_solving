d31a5a5230cf7bdd04938a71f1bd57f6aab76b00
nazmulmilon <milonsarker73@gmail.com>
2022-02-11 16:15:01 +0600

    class&object example

---
+++ Data_structure& Algorithm/space_complexity/2.py
@@ -0,0 +1,15 @@
+# time_complexity==
+# space complexity== O(n)
+
+n=int(input("Enter a input:"))
+even[101]
+for i in range(0,i<=101):
+    even[i]=0
+
+for i in range(0, i<=101, 2):
+    even[i]=i
+
+if even[n]:
+    print(n+ "" "is even")
+else:
+    print(n+ "" "is odd")
---
+++ program/class&object.py
@@ -0,0 +1,7 @@
+class Person:
+    def __init__(self, name, age):
+        self.name=name
+        self.age=age
+P1=Person("Milon", 36)
+print(P1.name)
+print(P1.age)
