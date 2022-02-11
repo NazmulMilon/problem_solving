62797142bb378941c16ef591761a55b20d13c91d
nazmulmilon <milonsarker73@gmail.com>
2022-02-11 10:47:13 +0600

    complexity

---
+++ Data_structure& Algorithm/complexity.py
@@ -0,0 +1,13 @@
+
+'''a=1
+b=3
+for i in range(a, b+1):
+    print(i)'''
+
+n=int(input("Enter a number:"))
+count=0
+for i in range(0,n):
+    for j in range(0,n):
+        count=count+1
+
+print(n, count)
