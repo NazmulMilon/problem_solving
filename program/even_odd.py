f9fe4e929fa15cf2861eece26dfb50ad6855da72
nazmulmilon <milonsarker73@gmail.com>
2022-02-13 09:27:32 +0600

    if else code updated

--- program/even_odd.py
+++ program/even_odd.py
@@ -1,4 +1,18 @@
 n = int(input("Enter a number: "))
+'''
+if n % 2 == 0:
+    if n in range(2,6):
+        print("Not Weird")
+
+    elif n in range(6,21):
+        print("Weird")
+
+    elif n > 20:
+        print("Not Weird")
+else:
+    print("Weird")
+'''
+
 if n % 2 == 0:
     if 2 <= n <= 5:
         print("Not Weird")
