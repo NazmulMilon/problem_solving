c27b7056bb5905ccbaf2faf44dc5be99be54675d
nazmulmilon <milonsarker73@gmail.com>
2022-02-13 09:06:08 +0600

    arithmetic code added

--- program/add_sum_multiply.py
+++ program/add_sum_multiply.py
@@ -1,7 +1,17 @@
 a, b = map(int, input().split())
 
-c = a + b
+
+'''c = a + b
 d = a - b
 e = a * b
 #print( c,"\n",d,"\n",e)
 print("%d\n%d\n%d"%(c,d,e))
+'''
+# print(f"{a+b}\n{a-b}\n{a*b}")
+# (lambda a, b : print((a + b), (a - b), (a * b), sep="\n"))(int(input()), int(input()))
+'''a,b = int(input()), int(input())
+print(*[a+b, a-b, a*b], sep="\n")'''
+
+print(a+b)
+print(a-b)
+print(a*b)
