66fa42f78c830d49760af8d4b98b655fec04c66f
nazmulmilon <milonsarker73@gmail.com>
2022-02-14 21:47:12 +0600

    hackerrank coding

--- program/hackerrank/4.division.py
+++ program/hackerrank/4.division.py
@@ -2,5 +2,13 @@ a,b = map(int, input("Enter two numbers: ").split())
 
 # a,b,c = map(int, input("Enter Three numbers").split())
 
-print(a//b)
+print(a//b,a/b,sep='\n')
+# print("{}\n{}".format(a//b, a/b))
+
+'''print(a//b)
 print(a/b)
+'''
+
+'''
+a,b=int(input()),int(input());print(a//b,a/b,sep='\n')
+'''
