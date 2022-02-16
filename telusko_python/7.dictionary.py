11f067395a1a02ac3b17200b1a08562fd0963310
nazmulmilon <milonsarker73@gmail.com>
2022-02-16 15:43:48 +0600

    list in python

---
+++ telusko_python/7.dictionary.py
@@ -0,0 +1,29 @@
+prog={'jss':'atom', 'CS':'VS', 'python':['pycharm', 'Sublime'], 'java':{'jse': 'netbeans', 'jee': 'ecilipse'}}
+print(prog)
+print(prog['jss'])
+print(prog['python'])
+print(prog['java'])
+
+print(prog['java']['jee'])
+
+'''
+key=['I', 'am', 'milon']
+value=['you', 'are', 'nazmul']
+data=dict(zip(key,value))
+print(data)
+
+data['hasan']='hasan'
+print(data)
+
+del data['am']
+print(data)
+'''
+
+'''
+data={1:'Nazmul', 2:'Hasan', 5:'Milon'}
+print(data)
+print(data[2])
+
+print(data.get(3,'Not Found'))
+'''
+
