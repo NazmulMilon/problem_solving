06ec4fd0bbc3025ba3eed4c2c39ee93b94fb1cb2
nazmulmilon <milonsarker73@gmail.com>
2022-02-16 12:42:12 +0600

    python list methods

---
+++ telusko_python/5.list.py
@@ -0,0 +1,51 @@
+nums=[4,6,2,1,9]
+name=['milon', 'roni', 'rupol', 'joy']
+
+print(nums)
+print(name)
+print([nums, name])
+nums.append(33)
+print(nums)
+nums.insert(2,55)
+print(nums)
+
+nums.remove(55)
+print(nums)
+
+nums.pop(1)
+print(nums)
+
+nums.pop()
+print(nums)
+
+del nums[2:]
+print(nums)
+
+nums.extend([24,27,36,78])
+print(nums)
+
+print(min(nums))
+print(max(nums))
+nums.sort()
+print(nums)
+
+'''
+In this program we will see :
+- Working with List
+- What is list
+- How to use list
+- Assign multiple values to variable
+- Printing value of list
+- Print value of list by index number
+- String list
+- List of different types of data types
+- Creating list with list : nested list
+- append function
+- insert function
+- remove function
+- pop function
+- delete function
+- extend function
+- Inbuilt functions of List : min, max, sum
+- sort function
+'''
