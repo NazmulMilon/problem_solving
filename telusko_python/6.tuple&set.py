6133859aa4bc1219814c28d56b13dec0d71dfc4d
nazmulmilon <milonsarker73@gmail.com>
2022-02-16 14:34:48 +0600

    tuple and set

---
+++ telusko_python/6.tuple&set.py
@@ -0,0 +1,12 @@
+
+'''
+ Difference between Tuple and List
+- Create Tuple - Tuple function - Count - Where to use Tuple
+- What is set - Define set - Hashing in set - Set functions
+ '''
+
+tup=(24,22,14,13)
+print(tup[1])
+
+s={29,14,3,9,100}
+print(s)
