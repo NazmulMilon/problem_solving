f648f4ff72eff1e1eb810e7c9323736bb1b09400
nazmulmilon <milonsarker73@gmail.com>
2022-02-20 08:36:53 +0600

    function code

---
+++ program/practice/function.py
@@ -0,0 +1,22 @@
+a=int(input("Enter a number: "))
+b=int(input("Enter a number:"))
+
+def large(a,b):
+    if a>b:
+        return a
+    else:
+        return b
+
+
+print(large(a,b))
+
+
+'''def add(a,b):
+    if a>b:
+        return a
+    else:
+        return b
+
+result=add(10,20)
+print(result)
+'''
