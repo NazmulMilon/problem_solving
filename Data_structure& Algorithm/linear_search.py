22a794cc183d6c50d6d3edfc8a7ecfd0045c95a6
nazmulmilon <milonsarker73@gmail.com>
2022-02-22 07:20:31 +0600

    linerar search using for loop

--- Data_structure& Algorithm/linear_search.py
+++ Data_structure& Algorithm/linear_search.py
@@ -1,7 +1,23 @@
-pos=-1
+def linear_search(array, n ,x):
+    for i in range(0,n):
+        if array[i]==x:
+            return i
+    return -1
+
+
+array=[2, 15, 16, 14, 9, 0]
+x=int(input("Enter the finding number: "))
+#x=15
+n=len(array)
+result=linear_search(array, n, x)
+if result==-1:
+    print("Not Found")
+else:
+    print("Element found at:", result+1)
+'''pos=-1
 def search(list, n):
     i=0
-
+    pos=-1
     while i<len(list):
         if list[i] == n:
             globals()['pos']=i
@@ -17,3 +33,4 @@ if search(list, n):
     print("Found at=", pos+1)
 else:
     print("Not Found")
+'''
