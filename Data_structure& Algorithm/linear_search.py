bd0bdcf036d98cc7c48d8abd4d1b15b53c175fc9
nazmulmilon <milonsarker73@gmail.com>
2022-02-22 09:20:25 +0600

    linear search updated

--- Data_structure& Algorithm/linear_search.py
+++ Data_structure& Algorithm/linear_search.py
@@ -1,9 +1,33 @@
-def linear_search(array, n ,x):
+
+lst=[]
+num=int(input("Enter the size of list: \t"))
+
+for n in range(num):
+    elements=int(input("Enter elements: \t"))
+    lst.append(elements)
+#print(lst)
+
+found=False
+
+x=int(input("Eneter a number to search: "))
+
+for i in range(len(lst)):
+    if x == lst[i]:
+        found=True
+        # print(f" '{x}' Element found at positon '{i+1}' " .format(x, i+1))
+        print(" '{}' Element found at positon '{}' ".format(x, i + 1))
+        break
+
+if not found:
+    print(" %d Elements not found in the list" %x)
+
+
+
+'''def linear_search(array, n ,x):
     for i in range(0,n):
         if array[i]==x:
             return i
     return -1
-
 
 array=[2, 15, 16, 14, 9, 0]
 x=int(input("Enter the finding number: "))
@@ -14,6 +38,9 @@ if result==-1:
     print("Not Found")
 else:
     print("Element found at:", result+1)
+
+'''
+
 '''pos=-1
 def search(list, n):
     i=0
--- program/practice/list_input.py
+++ program/practice/list_input.py
@@ -1,7 +1,24 @@
+
+
 list=[]
+n=int(input("Enter elements:"))
+for i in range(0, n):
+    ele=int(input())
+    list.append(ele)
+print(list)
+
+
+
+
+
+
+
+
+'''list=[]
 n=int(input("Enter elements of number: "))
 
 for i in range(0,n):
     ele=int(input())
     list.append(ele)
 print(list)
+'''
