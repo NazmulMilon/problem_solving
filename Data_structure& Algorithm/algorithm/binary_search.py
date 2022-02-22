04b70ffd2064dae08c59f8049c28fb55dd102505
nazmulmilon <milonsarker73@gmail.com>
2022-02-22 10:52:17 +0600

    binary search

---
+++ Data_structure& Algorithm/algorithm/binary_search.py
@@ -0,0 +1,25 @@
+pos=-1
+
+
+def binary_search(list, n):
+    l=0
+    u=len(list)-1
+    while l<=u:
+        mid=(l+u)//2
+        if list[mid]==n:
+            globals()['pos']=mid
+            return True
+        else:
+            if list[mid]<n:
+                l=mid+1
+            else:
+                u=mid-1
+    return False
+
+list = [5, 4, 9, 12, 44, 55]
+n=55
+
+if binary_search(list, n):
+    print("Found at: ", pos+1)
+else:
+    print("Not found")
