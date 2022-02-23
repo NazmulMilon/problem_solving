ecc8f64d419df013885def808814dd2749c3b343
nazmulmilon <milonsarker73@gmail.com>
2022-02-23 22:03:46 +0600

    print function updated

---
+++ program/hackerrank/7.print_function.py
@@ -0,0 +1,11 @@
+
+for i in range(1, 1+int(input())): print(i, end="")
+
+
+# print(*range(1, int(input("Enter a Number: "))+1), sep='')
+
+'''n=int(input("Enter an integer number: "))
+for i in range(1, n+1):
+    print(i, end='')
+'''
+
