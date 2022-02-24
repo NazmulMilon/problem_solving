02db84ad49b0e45151588302d4506908b97c035e
nazmulmilon <milonsarker73@gmail.com>
2022-02-24 11:34:00 +0600

    polymorphism

---
+++ program/hackerrank/8.runner_up_score.py
@@ -0,0 +1,3 @@
+n=int(input("Enter a number: "))
+array=map(int,input().split())
+print(array)
---
+++ program/practice/oop/polymorphism.py
@@ -0,0 +1,14 @@
+
+# Build in Polymorphism
+print(len("Md. Milon Sarker"))
+print(len([10,20,30,40,50]))
+
+
+# user defined polymorphism
+
+def add(x, y, z=0):
+    return x+y+z
+
+
+print(add(15,16))
+print(add(12,13,14))
