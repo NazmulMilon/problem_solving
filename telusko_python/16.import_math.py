0f606290ebe10498d5c93f6fc6bceee6b1172d12
nazmulmilon <milonsarker73@gmail.com>
2022-03-03 11:35:20 +0600

    import math

---
+++ telusko_python/16.import_math.py
@@ -0,0 +1,24 @@
+import math
+import math as m
+from math import sqrt, pow
+x=math.sqrt(25)
+print(x)
+# print(math.sqrt(25))
+
+print(math.sqrt(15))
+
+print(math.floor(2.9))
+print(math.floor(2.1))
+
+print(math.ceil(9.1))
+
+print(math.pow(5, 2))
+
+print(math.pi)
+
+print(math.e)
+
+print(m.sqrt(25))
+
+print(math.pow(12, 2))
+print(help('math'))
