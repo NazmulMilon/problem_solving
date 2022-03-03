7cff5307ca6ab716380311f334f07e408aa94c35
nazmulmilon <milonsarker73@gmail.com>
2022-03-03 19:28:19 +0600

    unit testing

---
+++ program/practice/mateen/test.py
@@ -0,0 +1,14 @@
+import unittest
+from unittest_example import add, is_even
+
+class MyTest(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+
+    def test_is_even(self):
+        self.assertTrue(is_even(2))
+
+
+if __name__ == '__main__':
+    unittest.main()
