f0888f184df80a866364a40a360de6f0ccf56d46
nazmulmilon <milonsarker73@gmail.com>
2022-03-05 16:27:30 +0600

    user input

---
+++ telusko_python/18.user_input.py
@@ -0,0 +1,14 @@
+'''
+ch=input('enter a char:')
+print(ch)
+
+ch=input('enter a char:')
+print(ch[0])
+
+ch=input('enter a char:')[0]
+print(ch)
+
+'''
+
+result = eval(input("Enter an expression: "))
+print(result)
