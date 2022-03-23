c696302b5ac59d3bd29fa8ecb2bbb8b8423fdc1a
nazmulmilon <milonsarker73@gmail.com>
2022-03-23 09:40:23 +0600

    pattern

---
+++ telusko_python/23. Pattern.py
@@ -0,0 +1,19 @@
+n=int(input("Enter a number: "))
+for i in range(n-1, ):
+    print(n)
+
+
+
+'''n=int(input("Enter a number: "))
+for i in range(n):
+    for j in range(n-i):
+        print('#', end='')
+    print()
+'''
+
+'''n=int(input("Enter a number: "))
+for i in range(n):
+    for j in range(i+1):
+        print('#', end="")
+    print()
+'''
