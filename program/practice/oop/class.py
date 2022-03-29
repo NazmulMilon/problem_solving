bc3afa8bf12f10104c631e2a0882a55d544a2bd4
nazmulmilon <milonsarker73@gmail.com>
2022-03-29 19:11:14 +0600

    class in oop using python

---
+++ program/practice/oop/class.py
@@ -0,0 +1,41 @@
+
+class Student:
+    roll=""
+    gpa=""
+    marks=""
+    name=""
+    district=""
+
+
+rahim=Student()
+rahim.roll=22
+rahim.gpa=3.5
+rahim.marks=70
+rahim.district='Dhaka'
+print(f"Roll:{rahim.roll}, GPA:{rahim.gpa}, Marks: {rahim.marks}, District: {rahim.district}")
+
+
+milon = Student()
+milon.roll=11
+milon.gpa=5.5
+milon.marks=44
+milon.district='Chandpur'
+print(f"Roll: {milon.roll}, GPA: {milon.gpa}, Marks: {milon.marks}, District: {milon.district}")
+
+
+'''class Student:
+    roll = ""
+    gpa = ""
+
+
+rahim = Student()
+rahim.roll = 45
+rahim.gpa = 4.5
+print(f"Roll: {rahim.roll}, GPA: {rahim.gpa}")
+
+
+karim = Student()
+karim.roll = 66
+karim.gpa = 33.22
+print(f"Roll: {karim.roll}, GPA: {karim.gpa}")
+'''
