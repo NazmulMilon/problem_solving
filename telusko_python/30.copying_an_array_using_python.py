7936a8170d5cd82152d115eb4dda8daeea59a3da
nazmulmilon <milonsarker73@gmail.com>
2022-04-05 11:49:21 +0600

    perform numpy different operations

--- telusko_python/30.copying_an_array_using_python.py
+++ telusko_python/30.copying_an_array_using_python.py
@@ -1,11 +1,32 @@
 from numpy import *
 
+arr1 = array([5, 3, 6, 9, 4])
+arr2 = array([9, 2, 4, 6, 9])
+print(concatenate([arr1, arr2]))
 
+
+'''
+arr = array([5, 3, 6, 5, 6,  9, 4, 8, 9])
+
+# print(sin(arr))
+# print(cos(arr))
+# print(sqrt(arr))
+# print(sum(arr))
+# print(max(arr))
+# print(min(arr))
+# print(sort(arr))
+print(unique(arr))
+'''
+
+
+
+'''
 # vectorized operation/ add two array
 arr1 = array([5, 3, 6, 9, 4])
 arr2 = array([9, 2, 4, 6, 9])
 arr3 = arr1 + arr2
 print(arr3)
+'''
 
 '''# add 5 to each elements
 arr = array([2, 4, 5 ,6, 7])
