edc6b0e32c103dbe3e6fbdb5e4f20a37ecc23742
nazmulmilon <milonsarker73@gmail.com>
2022-04-05 10:43:39 +0600

    using numpy did array, linspace, arange

---
+++ telusko_python/29.create_array_using_numpy.py
@@ -0,0 +1,24 @@
+from numpy import *
+
+'''# arr= arange(1, 20, 3)
+arr=arange(1, 20, 2)
+print(arr)
+'''
+
+'''
+#using linspace
+#arr = linspace(1, 10, 15)
+#arr=linspace(1, 20, 22)
+#arr = linspace(1, 20, 5)
+arr=linspace(0, 15, 16)
+print(arr)
+'''
+
+'''
+# using array() 
+arr = array([1, 3, 4 ,6,7, 9], float)
+arr = array([1, 3, 4 ,6,7, 9], int)
+
+print(arr.dtype)
+print(arr)
+'''
