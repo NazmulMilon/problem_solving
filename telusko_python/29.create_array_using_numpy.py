ec0719207baf9b40d8e22cd8ac7b5d55010baef9
nazmulmilon <milonsarker73@gmail.com>
2022-04-05 11:09:42 +0600

    logspace, zeros, ones done using numpy

--- telusko_python/29.create_array_using_numpy.py
+++ telusko_python/29.create_array_using_numpy.py
@@ -1,7 +1,23 @@
 from numpy import *
 
-'''# arr= arange(1, 20, 3)
-arr=arange(1, 20, 2)
+
+arr = ones(5)
+print(arr)
+
+'''arr = zeros(5)
+print(arr)
+'''
+
+'''
+# arr= logspace(1, 40, 5)
+# print(arr)
+arr = logspace(1, 40, 5)
+print('%.2f' %arr[2])
+'''
+
+'''
+# arr= arange(1, 20, 3)
+#arr=arange(1, 20, 2)
 print(arr)
 '''
 
@@ -10,7 +26,9 @@ print(arr)
 #arr = linspace(1, 10, 15)
 #arr=linspace(1, 20, 22)
 #arr = linspace(1, 20, 5)
-arr=linspace(0, 15, 16)
+#arr=linspace(0, 15, 16)
+arr=linspace(1, 15) #it will make default 50 value
+
 print(arr)
 '''
 
