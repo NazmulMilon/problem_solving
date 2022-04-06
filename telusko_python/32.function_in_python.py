fdb34803ca21439db482925db1ccb534df60c0a1
nazmulmilon <milonsarker73@gmail.com>
2022-04-06 08:49:06 +0600

    function in python

---
+++ telusko_python/32.function_in_python.py
@@ -0,0 +1,37 @@
+
+def add_sub(a, b):
+    c = a+b
+    d = a-b
+    return c, d
+
+
+result1, result2 = add_sub(8, 4)
+print(result1, result2)
+
+'''def add_sub(a, b):
+    c=a+b
+    d=a-b
+    print(c, d)
+
+# add_sub(5, 4)
+result = add_sub(5, 4)
+# result1,result2 = add_sub(5, 4)
+# print(result1, result2)
+'''
+
+
+def add(a, b):
+    sum = a+b
+    print(sum)
+
+add(5, 4)
+
+'''
+def greet():
+    print("Hello")
+    print("Good Morning")
+
+
+greet()
+'''
+
