4cfbf877595f84d0ec12252d46139b119294f829
nazmulmilon <milonsarker73@gmail.com>
2022-04-06 07:57:59 +0600

    dimensional array

--- telusko_python/31.matrix_using_numpy.py
+++ telusko_python/31.matrix_using_numpy.py
@@ -1,5 +1,41 @@
 from numpy import *
 
+
+# m = matrix('2 3 4; 5 6 5; 7 8 9 ')
+m = matrix('2 3; 4 5; 6 5; 7 8')
+
+
+print(m)
+
+'''
+arr1 = array([
+
+    [2, 3, 4, 5],
+    [7, 8, 9, 0],
+])
+
+# m=matrix(arr1)
+# m = matrix('2, 3, 4, 5; 7, 8, 9, 0')
+m = matrix('2 3 4 5; 7 8 9 0')
+
+print(m)
+'''
+
+'''
+arr1 = array([
+
+    [2, 3, 4, 5, 6, 2],
+    [7, 8, 9, 0, 1, 3],
+])
+
+arr2 = arr1.flatten()
+# arr3 = arr1.reshape(3, 4) # reshape the matrix
+arr3 = arr1.reshape(2,2,3)
+
+print(arr3)
+'''
+
+'''
 arr1 = array([
 
     [2, 3, 4, 5, 6, 2],
@@ -8,6 +44,7 @@ arr1 = array([
 
 arr2 = arr1.flatten()
 print(arr2)
+'''
 
 
 '''
