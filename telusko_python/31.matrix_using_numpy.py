1af711228e500feae0082bee73c8ad183353bb80
nazmulmilon <milonsarker73@gmail.com>
2022-04-06 07:41:54 +0600

    matrix using numpy

---
+++ telusko_python/31.matrix_using_numpy.py
@@ -0,0 +1,25 @@
+from numpy import *
+
+arr1 = array([
+
+    [2, 3, 4, 5, 6, 2],
+    [7, 8, 9, 0, 1, 3],
+])
+
+arr2 = arr1.flatten()
+print(arr2)
+
+
+'''
+arr1 = array([
+
+    [2, 3, 4, 5, 6],
+    [7, 8, 9, 0, 1],
+])
+
+#print(arr1)
+# print(arr1.dtype)
+#print(arr1.ndim)
+print(arr1.shape)
+print(arr1.size)
+'''
