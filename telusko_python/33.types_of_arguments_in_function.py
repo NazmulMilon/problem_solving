9bcf677c59be7657926f8cc8c2676bd85220a64c
nazmulmilon <milonsarker73@gmail.com>
2022-04-07 11:10:17 +0600

    position and keyword argument

---
+++ telusko_python/33.types_of_arguments_in_function.py
@@ -0,0 +1,20 @@
+
+
+'''
+# keyword arguments
+def person(name, age):
+    print(name, age)
+    
+person(name='milon', age=19)
+'''
+
+
+'''
+# position arguments
+def person(name, age):
+
+    print(name, age)
+
+
+person('milon', 28)
+'''
