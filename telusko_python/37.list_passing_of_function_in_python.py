37eec994ff2691f62f978c434cd9c906887a46b5
nazmulmilon <milonsarker73@gmail.com>
2022-04-08 12:07:05 +0600

    passing list using function assignment done

--- telusko_python/37.list_passing_of_function_in_python.py
+++ telusko_python/37.list_passing_of_function_in_python.py
@@ -1,5 +1,30 @@
 
-def count_fn(lst):
+# assignment:: Take 10 names from user then count and display number of users who has length more than 5 letters.
+
+
+def display(user_lst):
+    count = 0
+    not_count = 0
+    for i in user_lst:
+        if len(i)>5:
+            count+=1
+
+
+        else:
+            not_count+=1
+
+    return count
+
+
+user_lst = ['milonn', 'nazmul', 'pk', 'mk', 'gk', 'roni', 'bayejid', 'rupol', 'net', 'sector']
+
+
+c=display(user_lst)
+print('More than 5 letters in word: ', c)
+
+
+
+'''def count_fn(lst):
     even = 0
     odd =0
 
@@ -15,4 +40,4 @@ lst = [14, 25, 44, 57, 23, 49, 90, 67, 54]
 
 
 even, odd = count_fn(lst)
-print(f"Even: {even}, Odd: {odd}")
+print(f"Even: {even}, Odd: {odd}")'''
