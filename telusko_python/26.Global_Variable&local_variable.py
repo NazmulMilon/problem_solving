6ebfc839b8004761af52259a0473aa292cc0508c
nazmulmilon <milonsarker73@gmail.com>
2022-04-08 11:38:57 +0600

    global and local variable

---
+++ telusko_python/26.Global_Variable&local_variable.py
@@ -0,0 +1,34 @@
+
+a = 15
+print(id(a))
+
+
+def local_variable():
+    a=20
+    x = globals()['a']
+    print(id(x))
+
+    print('local:', a)
+
+
+local_variable()
+
+
+print('Global:',a)
+
+
+'''
+a = 15
+
+
+def local_variable():
+    global a
+    a=20
+    print('local:', a)
+
+
+local_variable()
+
+
+print('Global:',a)
+'''
