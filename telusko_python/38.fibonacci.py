93cdc96379a9be5bdcc506eae316728166aa94c3
nazmulmilon <milonsarker73@gmail.com>
2022-04-09 07:35:09 +0600

    fibonacci number using function

--- telusko_python/38.fibonacci.py
+++ telusko_python/38.fibonacci.py
@@ -1,17 +1,55 @@
+n = int(input("Enter a number: "))
 
-n = int(input("Enter number: "))
+
+def fibonacci(n):
+    a=0
+    b=1
+    if n == 1:
+        print(a)
+    else:
+        print(a, end=" ")
+        print(b, end=" ")
+
+        for i in range(2, n):
+            c = a + b
+            print(c, end=" ")
+            a = b
+            b = c
+
+
+fibonacci(n)
+
+
+'''n = int(input("Enter a number: "))
+
+def fibonacci(n):
+    a=0
+    b=1
+    print(a, end=" ")
+    print(b, end=" ")
+
+    for i in range(2, n):
+        c=a+b
+        print(c, end=" ")
+        a=b
+        b=c
+
+
+fibonacci(n)
+'''
+'''n = int(input("Enter number: "))
 
 a=0
 b=1
 print(a, end=" ")
 print(b, end=" ")
 
-for i in range(n):
+for i in range(3, n+1):
     c= a+b
-    print(c, end=" ")
+    # print(c, end=" ")
 
     a=b
     b=c
 
 
-    # print(c, end=" ")
+    print(c, end=" ")'''
