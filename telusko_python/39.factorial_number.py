93e2464ef5b064e3b5b3a560968b7b832725bec1
nazmulmilon <milonsarker73@gmail.com>
2022-04-09 08:04:52 +0600

    factorial program using function

--- telusko_python/39.factorial_number.py
+++ telusko_python/39.factorial_number.py
@@ -1,6 +1,20 @@
 
 n = int(input("Enter a number: "))
+
+
+def factorial(n):
+    fact = 1
+    for i in range(1, n+1):
+        fact = fact*i
+    # print("Result is: ", fact)
+    return fact
+
+
+factorial(n)
+print(factorial(n))
+
+'''n = int(input("Enter a number: "))
 fact = 1
 for i in range(1, n+1):
     fact = fact*i
-print("Factorial of" , n ,"is:", fact)
+print("Factorial of" , n ,"is:", fact)'''
