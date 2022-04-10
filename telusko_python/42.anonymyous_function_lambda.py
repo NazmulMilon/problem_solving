2d0fbc15ad6eeaf31277687cf4f6b3fc65ef4a67
nazmulmilon <milonsarker73@gmail.com>
2022-04-10 11:13:14 +0600

    lambda function

---
+++ telusko_python/42.anonymyous_function_lambda.py
@@ -0,0 +1,18 @@
+
+a = int(input("Enter a number: "))
+
+m = lambda a:a*a
+result = m(a)
+print(result)
+
+
+'''m = lambda a: a*a
+result = m(5)
+print(result)'''
+
+
+'''a = int(input("Enter a number: "))
+
+m = lambda a:a*a
+print(m(a))'''
+
