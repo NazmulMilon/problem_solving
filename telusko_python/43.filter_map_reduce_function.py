eb9e3a0e7961d308c410b216071fa8a5d784f2e3
nazmulmilon <milonsarker73@gmail.com>
2022-04-11 19:41:49 +0600

    filter function using lambda function

--- telusko_python/43.filter_map_reduce_function.py
+++ telusko_python/43.filter_map_reduce_function.py
@@ -1,6 +1,18 @@
 
+#map function
 
-def is_even(n):
+
+
+
+'''# filter function using lambda function
+nums = [12, 34, 11, 14, 65, 78]
+
+evens = list(filter(lambda n:n%2==0, nums))
+print(evens)
+'''
+
+# filter function
+'''def is_even(n):
     return n%2==0
 
 
@@ -8,4 +20,4 @@ nums = [3, 2, 6, 8, 14, 16, 22, 9]
 
 evens = list(filter(is_even, nums))
 
-print(evens)
+print(evens)'''
