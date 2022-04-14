071c7b98b81d79f52e316bb0392238e67f1bcf65
nazmulmilon <milonsarker73@gmail.com>
2022-04-14 12:46:36 +0600

    module another example done

--- module/demo.py
+++ module/demo.py
@@ -6,10 +6,28 @@ b = 5
 c = add(a, b)
 print(c)'''
 
-import module
+'''import module
 
 a = 7
 b = 6
 
 c = module.add(a, b)
-print(c)
+print(c)'''
+
+
+from module import *
+
+a = 10
+b = 8
+
+c1 = add(a, b)
+print(c1)
+
+c2 = sub(a, b)
+print(c2)
+
+c3 = mul(a, b)
+print(c3)
+
+c4 = div(a, b)
+print(c4)
