892319e6e551c22a37eeccc94a18ac838d9cad49
nazmulmilon <milonsarker73@gmail.com>
2022-04-15 07:22:55 +0600

    special variable __name__

---
+++ special_variable-__name__/demo.py
@@ -0,0 +1,17 @@
+from module import add, sub
+
+def fun1():
+    add()
+    print("from fun1")
+
+
+def fun2():
+
+    print("from fun2")
+
+
+def main():
+    fun1()
+    fun2()
+
+main()
---
+++ special_variable-__name__/module.py
@@ -0,0 +1,16 @@
+
+
+def add():
+    print("Result of sum is: ")
+
+def sub():
+    print("Result of sub is:")
+
+
+def main():
+    add()
+    sub()
+
+
+if __name__ == '__main__':
+    main()
