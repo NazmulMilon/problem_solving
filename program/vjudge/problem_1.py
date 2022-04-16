12111faeed9df4df650279a915d375b015fb970b
nazmulmilon <milonsarker73@gmail.com>
2022-04-16 14:20:39 +0600

    programming contest problem solve in python

---
+++ program/vjudge/problem_1.py
@@ -0,0 +1,17 @@
+
+a = int(input("Enter number: "))
+sum=0
+sum1=0
+for i in range(1,a+1, a<=a+1):
+    if i%2==0:
+        sum=sum+i
+        # print(i)
+    else:
+        sum1=sum1+i
+
+result = sum-sum1
+print("result is:", result)
+
+
+# print(sum)
+# print(sum1)
