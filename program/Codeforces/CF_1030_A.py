5db50cd4b97ae87f65c8270379c268af25a23bd5
nazmulmilon <milonsarker73@gmail.com>
2022-04-16 21:59:28 +0600

    cf_1030_A program in differnet way

--- program/Codeforces/CF_1030_A.py
+++ program/Codeforces/CF_1030_A.py
@@ -1,7 +1,23 @@
 
 
-n = input()
+'''n = input()
 line = input()
 
 str1 = line.find("1") == -1 and "EASY" or "HARD"
-print(str1)
+print(str1)'''
+
+
+n = int(input())
+line = []
+
+for i in range(n):
+    a=int(input())
+    line.append(a)
+
+for j in range(len(line)):
+    if line[j]==1:
+        print("HARD")
+        break
+else:
+    print("EASY")
+
