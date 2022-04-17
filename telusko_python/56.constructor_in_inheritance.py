663b1ecb32c4232098e9754c3f3f6348fcc7eb76
nazmulmilon <milonsarker73@gmail.com>
2022-04-17 11:45:27 +0600

    Method Resolution Order(MRO) completed

--- telusko_python/56.constructor_in_inheritance.py
+++ telusko_python/56.constructor_in_inheritance.py
@@ -1,3 +1,39 @@
+# Method Resolution Order(MRO)
+class A:
+    def __init__(self):
+        print("in A Init")
+
+    def feature1(self):
+        print("Feature 1-A Working")
+
+    def feature2(self):
+        print("Feature 2 working")
+
+
+class B:
+    def __init__(self):
+        print("in B Init")
+#                     if you create object of subclass it will first try find
+#                     init of subclass. If it is not found then it will call init of super class.
+
+    def feature1(self):
+        print("Feature 1-B Working")
+
+    def feature4(self):
+        print("Feature 4 Working")
+
+
+class C(A, B):
+    def __init__(self):
+        super().__init__() # Method Resolution Order(MRO): When multiple inheritance it start to execute left to right
+        print("in C Init")
+
+
+a1 = C()
+a1.feature1()
+
+'''
+# Constructor in Inheritance
 class A:
     def __init__(self):
         print("in A Init")
@@ -13,8 +49,8 @@ class B(A):
     def __init__(self):
         super().__init__() # to call init method of class A, we need super() method
         print("in B Init")
-        '''if you create object of subclass it will first try find
-        init of subclass. If it is not found then it will call init of super class.'''
+        #if you create object of subclass it will first try find
+        # init of subclass. If it is not found then it will call init of super class.
 
     def feature3(self):
         print("Feature 3 Working")
@@ -26,3 +62,4 @@ class B(A):
 # a1 = A()
 
 b1 = B()
+'''
