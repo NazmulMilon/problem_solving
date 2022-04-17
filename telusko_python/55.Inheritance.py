dc168de3ff8c11770c712d61d8e3df15b979281c
nazmulmilon <milonsarker73@gmail.com>
2022-04-17 11:07:17 +0600

    single inheritance done

---
+++ telusko_python/55.Inheritance.py
@@ -0,0 +1,24 @@
+
+'''
+class A:
+    def feature1(self):
+        print("Feature 1 Working")
+
+    def feature2(self):
+        print("Feature 2 working")
+
+
+class B(A):
+    def feature3(self):
+        print("Feature 3 Working")
+
+    def feature4(self):
+        print("Feature 4 Working")
+
+
+b1 = B()
+b1.feature1()
+b1.feature2()
+b1.feature3()
+b1.feature4()
+'''
