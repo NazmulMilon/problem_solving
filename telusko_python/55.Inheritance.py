70d13f6625eedcf305006fffe62e57af1c3f1e96
nazmulmilon <milonsarker73@gmail.com>
2022-04-17 11:13:25 +0600

    multilevel Inheritance example

--- telusko_python/55.Inheritance.py
+++ telusko_python/55.Inheritance.py
@@ -1,5 +1,6 @@
 
 '''
+# Single Inheritance
 class A:
     def feature1(self):
         print("Feature 1 Working")
@@ -22,3 +23,33 @@ b1.feature2()
 b1.feature3()
 b1.feature4()
 '''
+
+'''
+# Multilevel Inheritance
+class A:
+    def feature1(self):
+        print("Feature 1 Working")
+
+    def feature2(self):
+        print("Feature 2 working")
+
+
+class B(A):
+    def feature3(self):
+        print("Feature 3 Working")
+
+    def feature4(self):
+        print("Feature 4 Working")
+
+
+class C(B):
+    def feature5(self):
+        print("Feature 5 Working")
+
+
+c1 = C()
+c1.feature1()
+c1.feature2()
+c1.feature3()
+c1.feature4()
+c1.feature5()'''
