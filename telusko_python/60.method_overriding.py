3b1faa7d9ec6ecdc702d8271528f8f6dfcb2c6e3
nazmulmilon <milonsarker73@gmail.com>
2022-04-20 11:49:28 +0600

    method overriding program

--- telusko_python/60.method_overriding.py
+++ telusko_python/60.method_overriding.py
@@ -1,5 +1,22 @@
 
-class A:
+class Phone:
+    def display(self):
+        print("I am in phone class")
+
+
+class Samsung(Phone):
+    # pass           # if no method call only pass it will print the value of Phone class
+    def display(self):
+        super().display()
+        print("I am in Samsung Class")
+
+
+s1 = Samsung()
+s1.display()
+
+
+
+'''class A:
 
     def show(self):
         print("in A Show")
@@ -11,4 +28,4 @@ class B(A):
 
 
 a1 = B()
-a1.show()
+a1.show()'''
