9d6688222b946e9830f02c095ae0848273eba5a2
nazmulmilon <milonsarker73@gmail.com>
2022-04-20 11:53:08 +0600

    method overriding using init/constructor method

--- telusko_python/60.method_overriding.py
+++ telusko_python/60.method_overriding.py
@@ -1,4 +1,20 @@
+# method overriding with constructor method
 
+class Phone:
+    def __init__(self):
+        print("I am in Phone Class")
+
+
+class Samsung(Phone):
+    def __init__(self):
+        super().__init__()
+        print("I am in Samsung Class")
+
+s1=Samsung()
+
+
+'''
+# method overriding with method
 class Phone:
     def display(self):
         print("I am in phone class")
@@ -12,8 +28,8 @@ class Samsung(Phone):
 
 
 s1 = Samsung()
-s1.display()
-
+#s1.display()
+'''
 
 
 '''class A:
