8c7629e64b10c92308cc9e458ace3ff0318b412f
nazmulmilon <milonsarker73@gmail.com>
2022-04-21 11:27:18 +0600

    another way of exception handling using finally

--- telusko_python/63.Exception_handling.py
+++ telusko_python/63.Exception_handling.py
@@ -1,5 +1,22 @@
 
 
+
+# another Way to handle exception using finally
+a=5
+b=2
+
+try:
+    print("resource open")
+    print(a/b)
+
+except Exception as e:
+    print("Hey, You can't divide a number by 0. ", e)
+
+finally:
+    print("resource closed.")
+
+
+'''
 # another Way to handle exception
 a=5
 b=0
@@ -10,7 +27,7 @@ except Exception as e:
     print("Hey, You can't divide a number by 0. ", e)
 
 print("Bye")
-
+'''
 
 '''a=5
 b=0
