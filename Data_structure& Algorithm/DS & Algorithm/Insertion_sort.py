bf1f537e0d6625fb890e58ffbe4d84db768d4c6d
nazmulmilon <milonsarker73@gmail.com>
2022-04-22 05:57:25 +0600

    insertion sort program code from code_basics

--- Data_structure& Algorithm/DS & Algorithm/Insertion_sort.py
+++ Data_structure& Algorithm/DS & Algorithm/Insertion_sort.py
@@ -1,5 +1,24 @@
+'''#code_basics
 
 
+def insertion_sort(arr):
+    for i in range(1, len(elements)):
+        anchor = elements[i]
+        j=i-1
+        while j>=0 and anchor<elements[j]:
+            elements[j+1] = elements[j]
+            j=j-1
+        elements[j+1] = anchor
+
+
+elements = [11, 9, 29, 7, 2, 15, 28]
+
+insertion_sort(elements)
+print(elements)
+'''
+
+'''
+# FelixTechTips (youtube channel)
 def insertion_sort(arr):
     for i in range(1, len(arr)):
         j=i
@@ -12,6 +31,7 @@ arr = [23, 2, 1, 76, 8, 19, 90, 80]
 
 insertion_sort(arr)
 print(arr)
+'''
 
 '''def insertion_sort(arr):
     for i in range(1, len(arr)):
