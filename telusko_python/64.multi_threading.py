eadf99897a132905c593739ff12759ebd279142f
nazmulmilon <milonsarker73@gmail.com>
2022-04-21 11:55:09 +0600

    multi threading pracices

--- telusko_python/64.multi_threading.py
+++ telusko_python/64.multi_threading.py
@@ -2,8 +2,31 @@
 from threading import *
 from time import sleep
 
-
 class Hello(Thread):
+    def run(self):
+        for i in range(5):
+            print("Hello")
+            sleep(0.5)
+
+class Hi(Thread):
+    def run(self):
+        for i in range(5):
+            print("Hi")
+            sleep(0.5)
+
+t1 = Hello()
+t2 = Hi()
+
+t1.start()
+sleep(0.2)
+t2.start()
+
+t1.join()
+t2.join()
+print("Bye")
+
+
+'''class Hello(Thread):
     def run(self):
         for i in range(5):
             print("Hello")
@@ -30,6 +53,7 @@ t1.join()
 t2.join()
 
 print("Bye")
+'''
 
 '''
 class Hello(Thread):
