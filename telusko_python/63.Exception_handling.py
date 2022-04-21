06c09a0e678b9d89e9cbcb4a623b787da0f6fa97
nazmulmilon <milonsarker73@gmail.com>
2022-04-21 11:21:29 +0600

    another way of exception handling

--- telusko_python/63.Exception_handling.py
+++ telusko_python/63.Exception_handling.py
@@ -1,6 +1,19 @@
 
+
+# another Way to handle exception
 a=5
-b=2
+b=0
+
+try:
+    print(a/b)
+except Exception as e:
+    print("Hey, You can't divide a number by 0. ", e)
+
+print("Bye")
+
+
+'''a=5
+b=0
 
 try:
     print(a/b)
@@ -8,3 +21,4 @@ try:
 except Exception:
     print("Hey, You can't divide a number by 0")
 print("Bye")
+'''
