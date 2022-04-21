c7034b655f78b2fcf06ec09cf2b243dddd7f7643
nazmulmilon <milonsarker73@gmail.com>
2022-04-21 11:50:00 +0600

    multi_threading program using join method

--- telusko_python/64.multi_threading.py
+++ telusko_python/64.multi_threading.py
@@ -2,6 +2,7 @@
 from threading import *
 from time import sleep
 
+
 class Hello(Thread):
     def run(self):
         for i in range(5):
@@ -24,3 +25,33 @@ t2 = Hi()
 t1.start()
 sleep(0.2)
 t2.start()
+
+t1.join()
+t2.join()
+
+print("Bye")
+
+'''
+class Hello(Thread):
+    def run(self):
+        for i in range(5):
+            print("Hello")
+            sleep(1)
+
+
+class Hi(Thread):
+    def run(self):
+        for i in range(5):
+            print("Hi")
+            sleep(1)
+
+
+t1 = Hello()
+t2 = Hi()
+
+# t1.run() #its not possible because run() method is a build in method of thread
+
+t1.start()
+sleep(0.2)
+t2.start()
+'''
