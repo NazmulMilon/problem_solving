ddd13c49fbe6ff674f3f222b85d55fa437164ffb
nazmulmilon <milonsarker73@gmail.com>
2022-04-21 06:18:38 +0600

    bubble sort algorithm program

--- Data_structure& Algorithm/DS & Algorithm/bubbld_sort.py
+++ Data_structure& Algorithm/DS & Algorithm/bubbld_sort.py
@@ -1,5 +1,22 @@
 
 
+def bubble_sort(arr):
+    for i in range(len(arr)-1):
+        for j in range(len(arr)-1-i):
+            if arr[j]>arr[j+1]:
+                temp=arr[j]
+                arr[j]=arr[j+1]
+                arr[j+1]=temp
+
+
+arr = list(map(int,input("Enter elements:").split()))
+
+
+bubble_sort(arr)
+print(arr)
+
+
+'''
 def bubble_sort(lst):
 
     for i in range(len(lst)):
@@ -15,3 +32,4 @@ lst = [4, 10, 25, 95, 14, 9, 59, 7]
 
 res=bubble_sort(lst)
 print(lst)
+'''
