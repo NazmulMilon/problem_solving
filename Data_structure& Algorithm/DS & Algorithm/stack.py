f5eae5c23293133c4457d048e02a861adf2cea76
nazmulmilon <milonsarker73@gmail.com>
2022-04-24 11:47:06 +0600

    stack program using class and method

--- Data_structure& Algorithm/DS & Algorithm/stack.py
+++ Data_structure& Algorithm/DS & Algorithm/stack.py
@@ -1,4 +1,69 @@
-books = []
+# Stack using class and object
+
+
+class Stack(object):
+    def __init__(self):
+        self.item = []
+
+    def push(self, item):
+        self.item.append(item)
+        return item
+
+    def display(self):
+        return self.item
+
+    def pop(self):
+        self.item.pop()
+        return
+
+    def peek(self):
+        if self.item:
+            return self.item[-1]
+        else:
+            return None
+
+    def size(self):
+        if self.item:
+            return len(self.item)
+        else:
+            return None
+
+    def is_empty(self):
+        if self.item == []:
+            return "Empty"
+        else:
+            return "Not Empty"
+
+
+
+if __name__ == "__main__":
+    st = Stack()
+    st.push("1")
+    st.push("2")
+    print(st.display())
+
+    print(st.size())
+    print(st.peek())
+    print(st.is_empty())
+
+    st.pop()
+    print(st.size())
+    print(st.peek())
+    print(st.is_empty())
+    st.pop()
+    print(st.is_empty())
+    print(st.size())
+
+    print(st.display())
+
+
+
+
+
+
+
+
+'''books = []
 
 books.append("Learn C")
 books.append("Learn C++")
@@ -18,6 +83,8 @@ print(books)
 books.pop()
 if not books:
     print("No books left")
+'''
+
 
 '''book = []
 book.append("Learn C")
