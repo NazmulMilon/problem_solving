36a63c67f1fa69667e74ba795b86bda113cfb07c
nazmulmilon <milonsarker73@gmail.com>
2022-04-24 10:30:35 +0600

    stack program for append, pop and empty program

--- Data_structure& Algorithm/DS & Algorithm/stack.py
+++ Data_structure& Algorithm/DS & Algorithm/stack.py
@@ -1,6 +1,25 @@
+books = []
 
+books.append("Learn C")
+books.append("Learn C++")
+books.append("Learn Java")
+books.append("Python")
+print(books)
 
-book = []
+books.pop()
+print(books)
+
+books.pop()
+print("New books is: ", books[-1])
+
+books.pop()
+print(books)
+
+books.pop()
+if not books:
+    print("No books left")
+
+'''book = []
 book.append("Learn C")
 book.append("Learn C++")
 book.append("Learn Java")
@@ -16,7 +35,7 @@ print("Last Item of book is: ", book[-1])
 print(book.pop())
 if not book:
     print("Books  not found in list.")
-
+'''
 
 '''book.insert(1, 'Python')
 print(book)'''
