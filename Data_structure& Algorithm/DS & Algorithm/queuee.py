cd1b60ef84b650a0a18deb9f3edad2e73143409b
nazmulmilon <milonsarker73@gmail.com>
2022-04-26 05:50:03 +0600

    queue using oop

--- Data_structure& Algorithm/DS & Algorithm/queuee.py
+++ Data_structure& Algorithm/DS & Algorithm/queuee.py
@@ -1,7 +1,57 @@
 from collections import deque
 
+class Queue(object):
+    def __init__(self):
+        self.item = []
 
-class Queue:
+    def enqueue(self, item):
+        self.item.insert(0, item)
+
+    def dequeue(self):
+        if self.item:
+            self.item.pop()
+        else:
+            return None
+
+    def peek(self):
+        if self.item:
+            return self.item[-1]
+
+    def display(self):
+        return self.item
+
+    def is_empty(self):
+        if self.item==[]:
+            return True
+        else:
+            return False
+
+    def size(self):
+        return len(self.item)
+
+
+if __name__ == "__main__":
+    qu = Queue()
+    qu.enqueue(item=1)
+    qu.enqueue(item=2)
+    qu.enqueue(item=5)
+    print(qu.display())
+    print("Size\t{}".format(qu.size()))
+    print("Peek\t{}".format(qu.peek()))
+
+
+    qu.dequeue()
+    print("Size\t{}".format(qu.size()))
+    print(qu.display())
+
+    print("Peek\t{}".format(qu.peek()))
+    print(qu.display())
+
+
+
+
+
+'''class Queue:
     def __init__(self):
         self.buffer = deque()
 
@@ -23,8 +73,8 @@ object.enqueue(val)
 object.dequeuee()
 object.is_empty()
 object.size(
+'''
 
-    
 '''bank = deque(['Nazmul', 'Hasan', 'Milon', 'Ahmed'])
 print(bank)
 
