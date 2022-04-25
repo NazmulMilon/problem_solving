4aa216c68cf6bc4686b1595d92881fabbdc6e5a3
nazmulmilon <milonsarker73@gmail.com>
2022-04-26 05:37:10 +0600

    queue program

--- Data_structure& Algorithm/DS & Algorithm/queuee.py
+++ Data_structure& Algorithm/DS & Algorithm/queuee.py
@@ -1,6 +1,31 @@
 from collections import deque
 
-bank = deque(['Nazmul', 'Hasan', 'Milon', 'Ahmed'])
+
+class Queue:
+    def __init__(self):
+        self.buffer = deque()
+
+    def enqueue(self, val):
+        self.buffer.appendleft(val)
+
+    def dequeuee(self):
+        return self.buffer.pop()
+
+    def is_empty(self):
+        return len(self.buffer) ==0
+
+    def size(self):
+        return len(self.buffer)
+
+
+object = Queue()
+object.enqueue(val)
+object.dequeuee()
+object.is_empty()
+object.size(
+
+    
+'''bank = deque(['Nazmul', 'Hasan', 'Milon', 'Ahmed'])
 print(bank)
 
 bank.popleft()
@@ -12,6 +37,7 @@ bank.popleft()
 
 if not bank:
     print("No person left")
+'''
 
 '''
 from collections import *
