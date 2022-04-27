7a6ae8d42ed2e88a26eb19251c0215abb661ed35
nazmulmilon <milonsarker73@gmail.com>
2022-04-27 14:31:48 +0600

    problem G codeforces

--- program/practice/Course/G.py
+++ program/practice/Course/G.py
@@ -2,7 +2,7 @@
 
 a,b,c = map(int, input().split())
 
-if (a==b and a==c) or (a!=b and b!=c):
+if (a==b and b==c) or (a!=b and b!=c and a!=c):
     print("No")
-elif(a==b and a!=c) or (a==c and a!=b) or (b==c and b!=a) or (b==a and b!=c) or (c==a and c!=b) or (c==b and c!=a):
+else:
     print("Yes")
