90f49d5674c9a33ad8a43ddced6f5cfcd0349979
nazmulmilon <milonsarker73@gmail.com>
2022-05-15 06:06:44 +0600

    file handling

--- telusko_python/65.file_handling/read_file.py
+++ telusko_python/65.file_handling/read_file.py
@@ -1,12 +1,25 @@
 
 f = open('sample.txt', 'r')
+f_out = open('word_count.txt', 'w')
 # print(f)
 # print(f.read())
 
 # print(f.readline(), end="#")
-print(f.readline())
+'''print(f.readline())'''
 # print(f.readline()) # will print second line
 # print(f.readline(5))
 
 
 # print(f.readlines())
+
+'''for line in f:
+    print(line)
+f.close()
+'''
+
+for line in f:
+    token = line.split(' ')
+    ##print(str(token))
+    f_out.write("Wordcount: "+ str(len(token))+" "+ line)
+    print(len(token))
+f.close()
