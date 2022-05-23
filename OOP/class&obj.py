3e2712c3b745aa4701928a9475b4e9674b1a7443
nazmulmilon <milonsarker73@gmail.com>
2022-05-23 12:26:33 +0600

    class and object something update

--- OOP/class&obj.py
+++ OOP/class&obj.py
@@ -9,9 +9,23 @@ class Person:
     def get_name(self):
         return self.name
 
+    def set_name(self, new_name):
+        if self.__has_any_number(new_name):
+            print("Sorry person name can't have number")
+            return
+        self.name = new_name
+
+    def __has_any_number(self, string):
+        return "0" in string
+
     def get_summary(self):
         return f"Name: {self.name}, DOB: {self.date_of_birth}, Height: {self.height}"
 
 
 a_person = Person("Milon", "1996", "%.5 feet")
 print(a_person.get_summary())
+a_person.set_name("Nazmul Milon")
+print(a_person.get_summary())
+
+print(a_person.date_of_birth) #this case there will no bracket (). because
+                                #bracket indicate that it's a function.
